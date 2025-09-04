c24267260c7e13afb97017306c1456cad1f8db2a
Park-seungg <parkis1101@gmail.com>
2025-09-04 10:15:34 +0900

    조회가 아닌 경우 응답할 때 resultCode와 msg 도입

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
-@RequestMapping("/api/v1/posts/{postId}/Comments")
+@RequestMapping("/api/v1/posts/{postId}/comments")
 @RequiredArgsConstructor
 @RestController
 public class ApiV1PostCommentController {
@@ -45,7 +47,7 @@ public class ApiV1PostCommentController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public String delete(
+    public Map<String, Object> delete(
             @PathVariable long postId,
             @PathVariable long id
     ) {
@@ -55,6 +57,10 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return "%d 댓글이 삭제 되었습니다.".formatted(id);
+        Map<String, Object> rsData = new LinkedHashMap<>();
+        rsData.put("resultCode", "200-1");
+        rsData.put("msg", "%d번 댓글이 삭제 되었습니다.".formatted(id));
+
+        return rsData;
     }
 }
