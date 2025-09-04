eb995c28d1373c796b4dda4ca7d588ab8cf5d2c9
Park-seungg <parkis1101@gmail.com>
2025-09-04 09:37:59 +0900

    액션 메서드들에 Transactional 붙여서 성공

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,7 +5,11 @@ import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,6 +19,7 @@ import java.util.List;
 public class ApiV1PostCommentController {
     private final PostService postService;
 
+    @Transactional(readOnly = true)
     @GetMapping
     public List<PostCommentDto> getItems(@PathVariable Long postId) {
         Post post = postService.findById(postId);
@@ -25,6 +30,7 @@ public class ApiV1PostCommentController {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     @GetMapping("/{id}")
     public PostCommentDto getItem(
             @PathVariable long postId,
@@ -37,6 +43,7 @@ public class ApiV1PostCommentController {
         return new PostCommentDto(postComment);
     }
 
+    @Transactional
     @GetMapping("/{id}/delete")
     public String delete(
             @PathVariable long postId,
