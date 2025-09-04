7f193ae700f8e6ea7e0535a7dff6bee14b7f6faf
Park-seungg <parkis1101@gmail.com>
2025-09-04 09:36:15 +0900

    댓글 삭제구현, Transactional 안붙여서 실패

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,10 +5,7 @@ import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class ApiV1PostCommentController {
 
     @GetMapping
     public List<PostCommentDto> getItems(@PathVariable Long postId) {
-        Post post = postService.getPost(postId);
+        Post post = postService.findById(postId);
 
         return post.getComments()
                 .stream()
@@ -33,10 +30,24 @@ public class ApiV1PostCommentController {
             @PathVariable long postId,
             @PathVariable long id
     ) {
-        Post post = postService.getPost(postId);
+        Post post = postService.findById(postId);
 
         PostComment postComment = post.findCommentById(id).get();
 
         return new PostCommentDto(postComment);
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(
+            @PathVariable long postId,
+            @PathVariable long id
+    ) {
+        Post post = postService.findById(postId);
+
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.deleteComment(post, postComment);
+
+        return "%d 댓글이 삭제 되었습니다.".formatted(id);
     }
 }
