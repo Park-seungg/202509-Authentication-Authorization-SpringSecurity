17489f54d418cf53cc18f841dc88e3147dcfab8a
Park-seungg <parkis1101@gmail.com>
2025-09-15 11:20:33 +0900

    GlobalExceptionHandler에서 ServiceException에 대한 처리를 추가

--- src/main/java/com/back/global/initData/golbalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/initData/golbalExceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.back.global.initData.golbalExceptionHandler;
 
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -56,6 +57,14 @@ public class GlobalExceptionHandler {
                         "400-1",
                         "JSON 형식이 올바르지 않습니다."
                 ),
+                BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<RsData<Void>> handle(ServiceException e) {
+        return new ResponseEntity<>(
+                e.getRsData(),
                 BAD_REQUEST
         );
     }
