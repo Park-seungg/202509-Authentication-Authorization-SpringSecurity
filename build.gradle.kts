d9f99a1fe3e35b3f883de3f6239a05c530ff3966
Park-seungg <parkis1101@gmail.com>
2025-09-18 11:18:36 +0900

    jjwt 라이브러리 추가, AuthTokenService/AuthTokenServiceTest 클래스 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<Test> {
