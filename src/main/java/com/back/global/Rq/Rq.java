30d15a91b49eb83795ff3b62ed0c175ef13fe4a8
Park-seungg <parkis1101@gmail.com>
2025-09-23 12:24:32 +0900

    쿠키에 Secure, SameSite=Strict, MaxAge=1년 으로 추가

--- src/main/java/com/back/global/Rq/Rq.java
+++ src/main/java/com/back/global/Rq/Rq.java
@@ -68,10 +68,13 @@ public class Rq {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/"); // 쿠키를 도메인 전체에서 쓰겠다.
         cookie.setHttpOnly(true); // 쿠키를 스크립트로 접근 못하게(XSS 공격방어)
+        cookie.setDomain("localhost"); // 쿠키가 적용될 도메인 지정
+        cookie.setSecure(true); // https 에서만 쿠키전송
+        cookie.setAttribute("SameSite", "Strict"); // 동일 사이트에서만 쿠키 전송(CSRF 공격방어)
 
-        if (value.isBlank()) {
-            cookie.setMaxAge(0);
-        }
+        // value가 빈 값이면 쿠키 삭제
+        if (value.isBlank()) {cookie.setMaxAge(0);
+        } else {cookie.setMaxAge(60 * 60 * 24 * 365);} // 1년
 
         resp.addCookie(cookie);
     }
